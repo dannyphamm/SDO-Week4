0ce66f7b0ccca9ef935d137c54088f0fc4306e33
s3719046 <37074069+s3719046@users.noreply.github.com>
2021-03-23 10:59:09 +1100

    Added junit reporter to unit tests

--- jest.config.js
+++ jest.config.js
@@ -97,6 +97,10 @@ module.exports = {
 
   // Use this configuration option to add custom reporters to Jest
   // reporters: undefined,
+  reporters: [
+    "default", 
+    "jest-junit"
+  ],
 
   // Automatically reset mock state between every test
   // resetMocks: false,
